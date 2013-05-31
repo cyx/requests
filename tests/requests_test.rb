a249e6453a8d19df44f67d22cf798b37e4b415c8
Cyril David <cyx@cyx.is>
2013-05-31 14:56:03 -0700

    Use single quotes instead of double quotes.

--- tests/requests_test.rb
+++ tests/requests_test.rb
@@ -1,37 +1,37 @@
 require 'requests/sugar'
 
-test "basic auth" do
-  r = Requests.get("http://httpbin.org/basic-auth/u/p", auth: ['u', 'p'])
+test 'basic auth' do
+  r = Requests.get('http://httpbin.org/basic-auth/u/p', auth: ['u', 'p'])
 
-  assert_equal r.json["authenticated"], true
-  assert_equal r.json["user"], "u"
+  assert_equal r.json['authenticated'], true
+  assert_equal r.json['user'], 'u'
 end
 
-test "GET" do
-  r = Requests.get("http://httpbin.org/get", params: { foo: "bar" })
+test 'GET' do
+  r = Requests.get('http://httpbin.org/get', params: { foo: 'bar' })
 
   assert_equal 200, r.status_code
-  assert_equal ["application/json"], r.headers["content-type"]
-  assert_equal "UTF-8", r.encoding.to_s
+  assert_equal ['application/json'], r.headers['content-type']
+  assert_equal 'UTF-8', r.encoding.to_s
 
-  assert(r.json["args"] && r.json["args"]["foo"] == "bar")
+  assert(r.json['args'] && r.json['args']['foo'] == 'bar')
 end
 
-test "POST data" do
-  r = Requests.post("http://httpbin.org/post", data: '{ "plan": "test" }')
+test 'POST data' do
+  r = Requests.post('http://httpbin.org/post', data: '{ 'plan': 'test' }')
 
   assert_equal 200, r.status_code
-  assert_equal ["application/json"], r.headers["content-type"]
-  assert_equal "UTF-8", r.encoding.to_s
+  assert_equal ['application/json'], r.headers['content-type']
+  assert_equal 'UTF-8', r.encoding.to_s
 
-  assert(r.json["json"]  && r.json["json"] == { "plan" => "test" })
+  assert(r.json['json']  && r.json['json'] == { 'plan' => 'test' })
 end
 
-test "PUT data" do
+test 'PUT data' do
 
 end
 
-test "POST params" do
+test 'POST params' do
   payload = [
     ['a[]', 'a1'],
     ['a[]', 'a2'],
@@ -39,11 +39,11 @@ test "POST params" do
     ['c', '4']
   ]
 
-  r = Requests.post("http://httpbin.org/post", data: payload)
+  r = Requests.post('http://httpbin.org/post', data: payload)
 
   assert_equal 200, r.status_code
 
-  form = r.json["form"]
+  form = r.json['form']
 
   assert_equal form['a[]'], ['a1', 'a2']
   assert_equal form['b'], '3'
